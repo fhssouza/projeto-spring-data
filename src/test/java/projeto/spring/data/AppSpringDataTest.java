4e0b64410bf6339675d4e9d1bc6f8d4e1b167300
Fábio Souza <62041832+fhssouza@users.noreply.github.com>
2022-05-09 16:34:14 -0300

    Criando nosso DAO Repository

--- src/test/java/projeto/spring/data/AppSpringDataTest.java
+++ src/test/java/projeto/spring/data/AppSpringDataTest.java
@@ -2,15 +2,23 @@ package projeto.spring.data;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.dao.InterfaceSpringDataUser;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
 public class AppSpringDataTest {
 
+	@Autowired
+	private InterfaceSpringDataUser interfaceSpringUser;
+
 	@Test
 	public void testeInsert() {
 		System.out.println("Iniciou spring com sucesso");
 	}
+	
+	
 }
